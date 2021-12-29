412ce694883a5557c954e9d1881378f59bfb1a59
lzn <lzn@certi.org.br>
2021-12-29 01:40:10 -0300

    initial commit

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import './styles.css'
-import logoImg from '../../assets/images/logo.svg'
+
+import LogoProffy from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg'
 import studyIcon from '../../assets/images/icons/study.svg'
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
 
-
 function Landing() {
     return (
         <div id="page-landing">
-            <div id="page-landing-content" className="container">
+            <div id="page-landing-content"className="container">
                 <div className="logo-container">
-                    <img src={logoImg} alt="Logo Proffy"/>
+                    <img src={LogoProffy} alt="Proffy Logo" />
                     <h2>Sua plataforma de estudos online.</h2>
                 </div>
 
@@ -23,17 +23,18 @@ function Landing() {
                 />
 
                 <div className="buttons-container">
-                    <a className="study">
+                    <a href="" className='study'>
                         <img src={studyIcon} alt="Estudar" />
                         Estudar
                     </a>
 
-                    <a className="give-classes">
-                        <img src={giveClassesIcon} alt="Ensinar" />
+                    <a href="" className='give-classes'>
+                        <img src={giveClassesIcon} alt="Dar aulas" />
                         Dar aulas
                     </a>
                 </div>
-                <span className="total-connections">
+
+                <span className='total-connections'>
                     Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
                 </span>
             </div>
