14563e4dfa1a73c0d91c5ebc4805cbeac15219c4
lzn <lzn@certi.org.br>
2022-01-11 22:57:18 -0300

    reconfiguration

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import RoutesPages from './routes';
+// import Landing from './pages/Landing';
 import './assets/styles/global.css';
-import Landing from './pages/Landing';
 
 function App() {
   return (
-    <Landing />
+      <RoutesPages />
   );
 }
 
